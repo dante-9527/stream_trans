a006beb15c90d05efd7da84ee79855e9d660810b
dante <oyj@yimudata.com>
2022-01-27 15:31:10 +0800

    add handler args func

--- DBrecv/mian.go
+++ DBrecv/mian.go
@@ -1,51 +1,75 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
 	"net"
-	"bufio"
 	"os"
 )
 
 // 定制log
-func setLogger(){
+func setLogger() {
 	fmt.Println("定制log")
 }
 
+// 处理命令行参数
+func HandleArgs() (string, int, string) {
+	var host string
+	var port int
+	var qpPath string
+	flag.StringVar(&host, "host", "0.0.0.0", "bind host")
+	flag.IntVar(&port, "port", 20001, "bind port")
+	flag.StringVar(&qpPath, "qp-path", "", "qp file path")
+
+	flag.Parse()
+
+	return host, port, qpPath
+}
 
 // 处理函数
-func process(conn net.Conn) {
+func process(conn net.Conn, qpPath string) {
 	defer conn.Close() // 关闭连接
-	read_size := 0
-	write_size := 0
+	fmt.Printf("%s has connected in", conn)
+	readSize := 0
+	writeSize := 0
+	// 创建qp文件
+	f, err := os.Create(qpPath)
+	defer f.Close()
+	if err != nil {
+		fmt.Println("err=", err)
+		return
+	}
 	for {
-		fmt.Printf("%s has connected in", conn)
 		reader := bufio.NewReader(conn)
-		writer := bufio.NewWriter(os.Stdout)
 		var buf [1024]byte
-		n, err := reader.Read(buf[:]) // 读取数据
+		readLength, err := reader.Read(buf[:]) // 读取数据
 		if err != nil {
 			fmt.Println("read from client failed, err:", err)
 			break
 		}
-		read_size += n
-		if n == 0 && err != nil{
+		readSize += readLength
+		if readLength == 0 && err != nil {
 			fmt.Println("read over")
-		}
-		nn, err := writer.Write(buf[:n])
-		if err != nil {
-			fmt.Println("write to stdout failed, err", err)
-		}
-		write_size += nn
-		if read_size == write_size{
-			fmt.Println("读写结束")
 			break
 		}
+		writeLength, err := f.Write(buf[:])
+		if err != nil {
+			fmt.Println("write to qppath failed, err:", err)
+			break
+		}
+		writeSize += writeLength
+	}
+	if readSize == writeSize{
+		fmt.Println("handler success")
 	}
 }
 
 func main() {
-	listen, err := net.Listen("tcp", "127.0.0.1:9999")
+	// 获取命令行参数
+	host, port, qpPath := HandleArgs()
+	// 建立连接
+	listen, err := net.Listen("tcp", fmt.Sprintf("%v:%d", host, port))
 	if err != nil {
 		fmt.Println("listen failed, err:", err)
 		return
@@ -56,6 +80,6 @@ func main() {
 			fmt.Println("accept failed, err:", err)
 			continue
 		}
-		go process(conn) // 启动一个goroutine处理连接
+		go process(conn, qpPath) // 启动一个goroutine处理连接
 	}
 }
--- DBsend/main.go
+++ DBsend/main.go
@@ -5,28 +5,53 @@ import(
 	"net"
 	"bufio"
 	"os"
+	"flag"
 )
 
+// ip地址格式钩子
+func VarIP(s string) (net.IP, int) {
+	ip := net.ParseIP(s)
+	if ip == nil {
+		return nil, 0
+	}
+	return ip,1
+	
+}
+
+// 处理命令行参数
+func HandleArgs() (string, int) {
+	var host string
+	var port int
+	flag.StringVar(&host, "host", "0.0.0.0", "bind host")
+	flag.IntVar(&port, "port", 20001, "bind port")
+	flag.Parse()
+
+	return host, port
+}
 
 // 客户端
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:9999")
+	host, port := HandleArgs()
+	conn, err := net.Dial("tcp", fmt.Sprintf("%v:%d",host,port))
 	if err != nil {
 		fmt.Println("Connect err:", err)
 		return
 	}
 	defer conn.Close() // 关闭连接
 	inputReader := bufio.NewReader(os.Stdin)
+	sendSize := 0
 	var buf [1024]byte
 	for {
-		n, err := inputReader.Read(buf[:]) // 读取stdin
+		_, err := inputReader.Read(buf[:]) // 读取stdin
 		if err != nil{
 			fmt.Println("read err", err)
 		}
-		_, err = conn.Write(buf[:n]) // 发送数据
+		n, err := conn.Write(buf[:]) // 发送数据
 		if err != nil {
 			return
 		}
-		fmt.Println(string(buf[:n]))
+		sendSize += n
+		
 	}
+	
 }
