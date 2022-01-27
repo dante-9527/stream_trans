c0eed7e128ede6a099bfeeba773e4c3c1301971c
dante <oyj@yimudata.com>
2022-01-27 18:28:29 +0800

    complate proto package

--- DBsend/main.go
+++ DBsend/main.go
@@ -1,12 +1,16 @@
 package main
 
-import(
+import (
+	"DBsend/proto"
+	"bufio"
+	"flag"
 	"fmt"
 	"net"
-	"bufio"
 	"os"
-	"flag"
+	"strconv"
 )
+
+const CHUNKSIZE = 2048
 
 // ip地址格式钩子
 func VarIP(s string) (net.IP, int) {
@@ -14,8 +18,8 @@ func VarIP(s string) (net.IP, int) {
 	if ip == nil {
 		return nil, 0
 	}
-	return ip,1
-	
+	return ip, 1
+
 }
 
 // 处理命令行参数
@@ -31,27 +35,52 @@ func HandleArgs() (string, int) {
 
 // 客户端
 func main() {
+	// 接收命令行参数
 	host, port := HandleArgs()
-	conn, err := net.Dial("tcp", fmt.Sprintf("%v:%d",host,port))
+	conn, err := net.Dial("tcp", fmt.Sprintf("%v:%d", host, port))
 	if err != nil {
 		fmt.Println("Connect err:", err)
 		return
 	}
 	defer conn.Close() // 关闭连接
-	inputReader := bufio.NewReader(os.Stdin)
+	reader := bufio.NewReader(os.Stdin)
 	sendSize := 0
-	var buf [1024]byte
+	var buf [CHUNKSIZE]byte
+	// 传输数据
 	for {
-		_, err := inputReader.Read(buf[:]) // 读取stdin
-		if err != nil{
+		n, err := reader.Read(buf[:]) // 读取stdin
+
+		if err != nil {
 			fmt.Println("read err", err)
 		}
-		n, err := conn.Write(buf[:]) // 发送数据
+		// 制作数据包
+		message, err := proto.Encode(buf[:])
+		if err != nil {
+			fmt.Println("encode error", err)
+		}
+		_, err = conn.Write(message) // 发送数据
 		if err != nil {
 			return
 		}
 		sendSize += n
-		
 	}
-	
+	// 接收回传
+	data, err := proto.Decode(reader)
+	// 判断服务端接收的数据是否与发送数据大小一致
+	if err != nil {
+		fmt.Println("error")
+		return
+	}
+	recvSize, err := strconv.Atoi(data)
+	if err != nil {
+		fmt.Println("error")
+		return
+	}
+	if recvSize == sendSize {
+		// 传输结束信号
+		flag := true
+		// 将flag转换成[]byte
+		flagMsg, err := proto.Encode(flag)
+	}
+
 }
